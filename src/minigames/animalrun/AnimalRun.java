7cb063b3ba3977ad726a041c03e2f2f07f0cd4b4
DESKTOP-A3I2IVA\Admin <test>
2025-09-09 20:13:42 +0200

    runGame Methode in AnimalRun

--- src/minigames/animalrun/AnimalRun.java
+++ src/minigames/animalrun/AnimalRun.java
@@ -7,8 +7,10 @@ import sas.View;
 import java.util.ArrayList;
 
 public class AnimalRun extends AbstractGame {
+    private  static final int FPS = 60;
     private static final int WIDTH = 900;
     private static final int HEIGHT = 700;
+    private boolean gameRuns = true;
 
     public AnimalRun(AbstractController controller, View view) {
         super(controller,view);
@@ -22,6 +24,25 @@ public class AnimalRun extends AbstractGame {
 
     @Override
     protected void runGame() {
+        long milisPerCycle = 1000 / FPS;
+        long timeStamp = System.currentTimeMillis();
+        long timeUntilNextCycle;
+
+        while (gameRuns) {
+            timeStamp = System.currentTimeMillis();
+            update();
+            timeUntilNextCycle = milisPerCycle - (System.currentTimeMillis() - timeStamp);
+            if (timeUntilNextCycle < 0) timeUntilNextCycle = 0;
+            try {
+                //Thread.sleep(timeUntilNextCycle);
+                view.wait(timeUntilNextCycle);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private void update() {
 
     }
 }
