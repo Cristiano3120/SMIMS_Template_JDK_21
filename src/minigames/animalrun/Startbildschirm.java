2b0746112d712d9d7b4f4b638418be2ed3d7a3c7
Nico <nico.eusepi@mein-gaussge.de>
2025-09-09 19:45:48 +0200

    renamed

--- src/minigames/animalrun/Startbildschirm.java
+++ src/minigames/animalrun/Startbildschirm.java
@@ -1,5 +1,7 @@
 package minigames.animalrun;
+
 import minigames.AbstractGame;
+import sas.Picture;
 import sas.Text;
 import sas.Tools;
 import sas.View;
@@ -7,16 +9,16 @@ import common.ScalablePicture;
 import controller.AbstractController;
 
 
-public class Startbildschirm extends AbstractGame{
+public class Startbildschirm extends AbstractGame {
 
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
 
+    ScalablePicture[] backgrounds;
+
     public Startbildschirm(AbstractController controller, View view) {
         super(controller, view);
-        ScalablePicture background = new ScalablePicture(0,0,view.getWidth(), view.getHeight(), "resources/animalrun/background.png");
-        ScalablePicture pressA_button = new ScalablePicture(200,250, 375, 250, "resources/animalrun/pressA_button.png");
-
+        initView();
     }
 
     @Override
@@ -24,10 +26,44 @@ public class Startbildschirm extends AbstractGame{
         view.setSize(WIDTH, HEIGHT);
         view.setName("Startbildschirm Animalrun");
 
+        ScalablePicture background = new ScalablePicture(0, 0, view.getWidth(), view.getHeight(), "resources/animalrun/background.png");
+        ScalablePicture pressA_button = new ScalablePicture(210, 300, 375, 250, "resources/animalrun/pressA_button.png");
+        ScalablePicture headline = new ScalablePicture(100, 0, 600, 400, "resources/animalrun/headline.png");
+
+        shapesToRemove.add(background);
+        shapesToRemove.add(pressA_button);
+        shapesToRemove.add(headline);
+
+        // hier auf button warten
+        while(!controller.getLinksA()) {
+            view.wait(10);
+        }
+
+
     }
 
     @Override
     protected void runGame() {
 
+        cleanUp();
+
+        // Generiere Backgrounds.
+        backgrounds = new ScalablePicture[4];
+        for (int i = 0; i < backgrounds.length; i++) {
+            backgrounds[i] = new ScalablePicture(i * 375.0, view.getHeight() - 100d, 375.0, 100, "resources/animalrun/background_1.png");
+            shapesToRemove.add(backgrounds[i]);
+        }
+
+    }
+
+    private void moveBackground(double offsetX) {
+        for (int j = 0; j < backgrounds.length; j++) { // Gehe durch alle Bodenelemente durch ...
+            backgrounds[j].move(-offsetX, 0); // ... und bewege das jeweils aktuelle Element nach links.
+            if (backgrounds[j].getShapeX() + backgrounds[j].getShapeWidth() < 0) { // Sind wir links über den Rand hinaus?
+                System.out.println("moving " + j);
+                int prevIndex = j <= 0 ? backgrounds.length - 1 : j - 1; // bestimme den Index des vorherigen Bodenelements
+                backgrounds[j].moveTo(backgrounds[prevIndex].getShapeX() + backgrounds[prevIndex].getShapeWidth(), backgrounds[j].getShapeY()); // "teleportiere" das ELement hinter seinen Vorgänger, sprich: ganz nach rechts
+            }
+        }
     }
 }
