6da52e8e46aea4fc61f5b33e4c560a201a5d4f92
DESKTOP-A3I2IVA\Admin <test>
2025-09-09 21:10:53 +0200

    AnimalRun, World und WorldObject Class

--- src/minigames/animalrun/AnimalRun.java
+++ src/minigames/animalrun/AnimalRun.java
@@ -2,15 +2,20 @@ package minigames.animalrun;
 
 import controller.AbstractController;
 import minigames.AbstractGame;
+import minigames.animalrun.Worldobject.WorldObject;
+import sas.Picture;
 import sas.View;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AnimalRun extends AbstractGame {
     private  static final int tickRate = 60;
     private static final int WIDTH = 900;
     private static final int HEIGHT = 700;
     private boolean gameRuns = true;
+
+    private World currentWorld;
 
     public AnimalRun(AbstractController controller, View view) {
         super(controller,view);
@@ -23,6 +28,7 @@ public class AnimalRun extends AbstractGame {
 
         new Startbildschirm(controller, view, WIDTH, HEIGHT);
 
+        currentWorld = new World(controller, view);
     }
 
     @Override
@@ -47,5 +53,25 @@ public class AnimalRun extends AbstractGame {
 
     private void update() {
 
+    }
+
+    private class World {
+        int xKameraVersatz = 0;
+        Picture background = new Picture(0,0,900,700,"resources/animalrun/background_game.png");
+
+        Set<WorldObject> wordObjects = new HashSet<WorldObject>();
+        // instanzen der playerklasse
+
+        public World(AbstractController controller, View view){
+
+        }
+
+        void update(int tick) {
+
+            wordObjects.stream().forEach(w ->{
+                w.doThings(tick);
+                w.updatePos();
+            });
+        }
     }
 }
--- src/minigames/animalrun/LVL/World_LVL1.java
+++
@@ -1,7 +0,0 @@
-package minigames.animalrun.LVL;
-
-import sas.Picture;
-
-public class World_LVL1 {
-    Picture background =new Picture(0,0,900,700,"resources/animalrun/background_game.png");
-}
