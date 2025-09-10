edba5f3f61b866f21558c8ae0424414a0afd084d
lill1y <spicyy0550@gmail.com>
2025-09-10 12:32:46 +0200

    Affen können laufen und springen

--- src/mastercontroler/Mastercontrol.java
+++ src/mastercontroler/Mastercontrol.java
@@ -17,8 +17,44 @@ public class Mastercontrol {
     protected AbstractGame game;
 
     public Mastercontrol() {
-        view = new View(WIDTH,HEIGHT);
-        controller =new TastaturController(view);
-        Monkey monkey = new Monkey(0, 200, 200, "C:\\Users\\Admin\\Downloads\\SMIMS_Template_JDK_21\\resources\\animalrun\\monkey1.jpeg", controller, true);
+        view = new View(WIDTH, HEIGHT);
+        controller = new TastaturController(view);
+
+        Monkey monkey = new Monkey(600, 200, 200, controller, true, view);
+        Monkey monkey2 = new Monkey(400, 200, 200, controller, false, view);
+
+        monkey.pictures1[0].hashCode();
+
+        loop(monkey, monkey2, view);
+
+    }
+
+    private void loop(Monkey monkey, Monkey monkey2, View view) {
+
+        monkey.pictures1[0].setHidden(true);
+        monkey2.pictures1[0].setHidden(true);
+
+        int i = 0;
+        while (true) {
+            monkey.monkeyMove();
+            monkey2.monkeyMove();
+            monkey.monkeyJump();
+            monkey2.monkeyJump();
+
+            monkey.pictures1[i].setHidden(true);
+            monkey2.pictures1[i].setHidden(true);
+
+            i = i >= 4 - 1
+                    ? 0
+                    : i + 1;
+
+            monkey.pictures1[i].setHidden(false);
+            monkey.currentIndex1 = i;
+
+            monkey2.pictures1[i].setHidden(false);
+            monkey2.currentIndex1 = i;
+
+            view.wait(180);
+        }
     }
 }
--- src/minigames/animalRun/Monkey.java
+++ src/minigames/animalRun/Monkey.java
@@ -1,7 +1,12 @@
 package minigames.animalRun;
 
+import common.ScalablePicture;
 import controller.AbstractController;
-import sas.Picture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sas.Circle;
+import sas.Shapes;
+import sas.View;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -9,90 +14,123 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-public class Monkey extends Picture {
+import static java.nio.file.Files.move;
 
+public class Monkey extends Circle {
+
+    private static final Logger log = LoggerFactory.getLogger(Monkey.class);
     /* Static Variables */
     protected static double MONKEY_MOVEMENT = 5.0;
     protected static int baseLevelX = 0;
 
     /* Object Variables */
     private AbstractController controller;
-    private String[]monkey1Pictures = new String[]{getDynamicFilePath("monkey1.jpeg"), getDynamicFilePath("monkey2.jpeg"), getDynamicFilePath("monkey3.jpeg"), getDynamicFilePath("monkey4.jpeg")};
-    private String[]monkey2Pictures = new String[]{getDynamicFilePath("monkey5.jpeg"), getDynamicFilePath("monkey6.jpeg"), getDynamicFilePath("monkey7.jpeg"), getDynamicFilePath("monkey8.jpeg")};
+    public final ScalablePicture[] pictures1 = new ScalablePicture[4];
+    public int currentIndex1;
     private boolean isMonkey1;
+    private boolean turnedLeft = true;
+    private View view;
 
     /* Constructors */
-    public Monkey(double yp, double w, double h, String name, AbstractController controller, boolean isMonkey1) {
-        super(baseLevelX, yp, w, h, "C:\\Users\\Admin\\Downloads\\SMIMS_Template_JDK_21\\resources\\animalrun\\monkey1.jpeg");
+    public Monkey(double yp, double w, double h, AbstractController controller, boolean isMonkey1, View view) {
+        super(100, yp , 0.1);
         this.controller = controller;
         this.isMonkey1 = isMonkey1;
+        this.view = view;
+        this.currentIndex1 = 0;
+        setHidden(true);
 
-        while (true) {
-            monkeyMove();
+        if (isMonkey1) {
+            setupMonkey1();
+        }
+        else {
+            setupMonkey2();
         }
     }
 
-    private String getDynamicFilePath(String fileName) {
-        return "C:\\Users\\Admin\\Downloads\\SMIMS_Template_JDK_21\\resources\\animalrun\\" + fileName;
+    public void setupMonkey1(){
+        for (int i = 1; i <= 4; i++){
+            ScalablePicture scalablePicture = new ScalablePicture(10, 200, 150, 150,"resources/animalrun/monkey" + (i) + ".png");
+            scalablePicture.setHidden(true);
+
+            pictures1[i-1] = scalablePicture;
+        }
+    }
+
+    private void setupMonkey2(){
+        for (int i = 1; i <= 4; i++){
+            ScalablePicture scalablePicture = new ScalablePicture(10, 200, 150, 150,"resources/animalrun/monkey" + (i + 4) + ".png");
+            scalablePicture.setHidden(true);
+
+            pictures1[i-1] = scalablePicture;
+        }
+    }
+
+    @Override
+    public boolean intersects(Shapes shape) {
+        return pictures1[currentIndex1].intersects(shape);
     }
 
     /* Object Methods */
-    public void monkeyMove(){
-        if (isMonkey1){
-            String tempPicture = monkey1Pictures[0];
-            System.out.println("Joystick: " + controller.getJoystickLinksY());
-            move(MONKEY_MOVEMENT * controller.getJoystickRechtsX());
-            setImageByString(monkey1Pictures[0]);
-
-            for (int g = 1; g < 3; g++){
-                monkey1Pictures[g-1] = monkey1Pictures[g];
-            }
-            monkey1Pictures[3] = tempPicture;
-        } else{
-            String tempPicture = monkey2Pictures[0];
-            move(MONKEY_MOVEMENT * controller.getJoystickLinksX());
-            setImageByString(monkey2Pictures[0]);
-            for (int g = 1; g < 4; g++){
-                monkey2Pictures[g-1] = monkey2Pictures[g];
-            }
-            monkey2Pictures[4] = tempPicture;
+    public void monkeyMove() {
+        if (isMonkey1) {
+            double joystickVal = controller.getJoystickRechtsX();
+            flip(joystickVal);
+        } else {
+            double joystickVal = controller.getJoystickLinksX();
+            flip(joystickVal);
         }
-
     }
 
-    public void monkeyJump(boolean isMonkey1){
-        if(isMonkey1){
-            double velocity = 0;
-            if ( controller.getRechtsA()){
-                velocity = 5;
-                while(velocity > -5){
-                    move(MONKEY_MOVEMENT * controller.getJoystickRechtsY(), velocity);
+    private void flip(double joystickVal) {
+        if (joystickVal < 0) {
+            if (!turnedLeft) {
+                for (ScalablePicture scalablePicture : pictures1) {
+                    scalablePicture.flipHorizontal();
                 }
-
+                turnedLeft = true;
             }
-        }else{
-            double velocity = 0;
-            if ( controller.getRechtsA()){
-                velocity = 5;
-                while(velocity > -5){
-                    move(MONKEY_MOVEMENT * controller.getJoystickLinksY(), velocity);
+        } else if (joystickVal > 0) {
+            if (turnedLeft) {
+                for (ScalablePicture scalablePicture : pictures1) {
+                    scalablePicture.flipHorizontal();
                 }
-        }   }
+                turnedLeft = false;
+            }
+        }
 
+        move(MONKEY_MOVEMENT * joystickVal);
+        for (int i = 0; i < 4; i++) {
+            pictures1[i].move(MONKEY_MOVEMENT * joystickVal * 6);
+        }
     }
 
-    void setImageByString(String str){
-        try {
-            Picture picture = new Picture(200, 200, str);
-            BufferedImage bufferedImage = ImageIO.read(new File(str));
-            //setImage(bufferedImage);
-           // setSize(bufferedImage.getWidth(), bufferedImage.getHeight());
-            super.loadTexture(str);
-            super.setImage(bufferedImage);
-            //bImage = ImageIO.read(new File(str));
-            //this.setImage(bImage);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void monkeyJump() {
+    if (isMonkey1) {
+            double velocity = 0;
+            if (controller.getRechtsA()) {
+                velocity = 200;
+                for (int i = 0; i < pictures1.length; i++) {
+                    pictures1[i].move(MONKEY_MOVEMENT * controller.getJoystickRechtsX() * 20, -velocity);
+                }
+                view.wait(200);
+                for (int i = 0; i < pictures1.length; i++) {
+                    pictures1[i].move(MONKEY_MOVEMENT * controller.getJoystickRechtsX() * 20, velocity);
+                }
+            }
+        } else {
+            double velocity = 0;
+        if (controller.getLinksA()) {
+            velocity = 200;
+            for (int i = 0; i < pictures1.length; i++) {
+                pictures1[i].move(MONKEY_MOVEMENT * controller.getJoystickLinksX() * 20, -velocity);
+            }
+            view.wait(200);
+            for (int i = 0; i < pictures1.length; i++) {
+                pictures1[i].move(MONKEY_MOVEMENT * controller.getJoystickLinksX() * 20, velocity);
+            }
         }
+        }
+
     }
 }
