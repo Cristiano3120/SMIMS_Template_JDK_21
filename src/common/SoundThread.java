094fe244109c386ef41d018721b00c36eeaa2997
may <michel-may@mailbox.org>
2025-09-11 13:28:57 +0200

    SoundThread dokumentiert

--- src/common/SoundThread.java
+++ src/common/SoundThread.java
@@ -7,6 +7,16 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * <p>Dies ist die mir einfachste Klasse, um Sounds bzw. Musik ins Spiel zu bekommen. Es handelt sich um einen Hintergrund-
+ * thread, damit das Spiel durch die Wiedergabe nicht blockiert wird. Verwendung:</p>
+ * <ul>
+ *     <li><code>SoundThread</code>-Objekt erstellen</li>
+ *     <li>wenn <code>loop = true</code>, dann wird die Datei automatisch wiederholt (z.B. für Hintergrundmusik)</li>
+ *     <li>{@link #start()}-Methode ausführen</li>
+ *     <li>ggf. mit {@link #stopPlayer()} frühzeitig beenden</li>
+ * </ul>
+ */
 public class SoundThread extends Thread {
 
     private FileInputStream inputStream;
@@ -14,11 +24,24 @@ public class SoundThread extends Thread {
     private Player player;
     private boolean loop;
 
-    private SoundThread(String file, boolean loop) throws FileNotFoundException, JavaLayerException {
+    /**
+     * Dieser Konstruktor erstellt ein neues Objekt vom Typ <code>SoundThread</code>.
+     *
+     * @param file   das zu verwendende sound file
+     * @param loop   gibt an, ob der Sound unendlich oft wiederholt werden soll.
+     * @param daemon gibt an, ob der Player als daemon thread behandelt werden soll. Das bedeutet: Ist der Wert auf
+     *               <code>false</code> gesetzt, dann wird das gesamte Programm erst dann beendet, wenn der Track vollständig
+     *               durchgelaufen ist. Setzt man hingegen <code>true</code>, dann wird der Thread automatisch 'gekillt',
+     *               wenn das Programm beendet wird.
+     * @throws FileNotFoundException falls das sound file nicht gefunden werden kann.
+     * @throws JavaLayerException    falls das {@link Player}-Objekt nicht erstellt werden kann.
+     */
+    private SoundThread(String file, boolean loop, boolean daemon) throws FileNotFoundException, JavaLayerException {
         this.file = file;
         this.inputStream = new FileInputStream(file);
         this.player = new Player(inputStream);
         this.loop = loop;
+        setDaemon(daemon);
     }
 
     public void run() {
@@ -36,6 +59,10 @@ public class SoundThread extends Thread {
         closeStream();
     }
 
+    public boolean isFinished() {
+        return player.isComplete();
+    }
+
     private void closeStream() {
 
         try {
@@ -47,7 +74,7 @@ public class SoundThread extends Thread {
         // Do we have to start a new player thread?
         if (loop) {
             try {
-                new SoundThread(file, loop).start();
+                new SoundThread(file, loop, isDaemon()).start();
             } catch (FileNotFoundException | JavaLayerException e) {
                 e.printStackTrace();
             }
